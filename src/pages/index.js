458f02db26753fbab12dc053931fe2d89bf9b9df
eidietrich <eric.dietrich@gmail.com>
2024-06-24 14:02:45 -0600

    Fix which interactive feature is showing

--- src/pages/index.js
+++ src/pages/index.js
@@ -124,9 +124,9 @@ export default function Home({ races, legislativeRaces, ballotIssues, text, voti
 
             <Markdown>{overviewLedeIn}</Markdown>
 
-            {/* <SearchForCandidate candidates={fullCandidateList} /> */}
+            <SearchForCandidate candidates={fullCandidateList} />
 
-            <AddressLookup selDistricts={selDistricts} setSelDistricts={setSelDistricts} />
+            {/* <AddressLookup selDistricts={selDistricts} setSelDistricts={setSelDistricts} /> */}
 
 
             <section>
