2d8926f2cc4cfc372615bb5bea724fbfea6ceee1
Jacob Olness <jolness1@gmail.com>
2024-09-10 17:06:15 -0600

    almost there

--- src/components/AddressLookup.js
+++ src/components/AddressLookup.js
@@ -1,11 +1,10 @@
 import { useState } from "react";
 import { css } from "@emotion/react";
 import mapDistrictCode from "../lib/mapDistrictCode";
+import DistrictFinder from '../lib/DistrictFinder';
 
-import DistrictFinder from '../lib/DistrictFinder'
-
-const PLACEHOLDER = 'Enter address (e.g., 1301 E 6th Ave, Helena)'
-const DEFAULT_MESSAGE = 'Look up districts for your address by entering it above.'
+const PLACEHOLDER = 'Enter address (e.g., 1301 E 6th Ave, Helena)';
+const DEFAULT_MESSAGE = 'Look up districts for your address by entering it above.';
 
 export default function AddressLookup({
     selDistricts,
@@ -14,6 +13,7 @@ export default function AddressLookup({
     const { usHouse, psc, mtHouse, mtSenate, matchedAddress } = selDistricts;
     const [value, setValue] = useState(null);
     const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(false);
 
     const districtFinder = new DistrictFinder();
 
@@ -25,13 +25,18 @@ export default function AddressLookup({
 
     function handleSubmit(event) {
         event.preventDefault();
+        setLoading(true);
         const result = districtFinder.matchAddressToDistricts(
             value,
             match => {
                 setSelDistricts(match);
                 setError(null);
+                setLoading(false);
             },
-            err => setError(` Invalid address, please make sure you enter it in this format: 1301 E 6th Ave, Helena`)
+            err => {
+                setError(` Invalid address, please make sure you enter it in this format: 1301 E 6th Ave, Helena`);
+                setLoading(false);
+            }
         );
     }
 
@@ -60,7 +65,9 @@ export default function AddressLookup({
             <div className="ledein">Show only candidates for your voting address</div>
             <form>
                 <input onChange={handleChange} type="address" value={value || ''} placeholder={PLACEHOLDER} />
-                <button onClick={handleSubmit}>Look up</button>
+                <button onClick={handleSubmit} disabled={loading} css={buttonStyle}>
+                    {loading ? 'Searching...' : 'Look up'}
+                </button>
             </form>
             <div className="message">
                 {error && (
@@ -70,13 +77,17 @@ export default function AddressLookup({
                 )}
                 {(matchedAddress === null && !error) && <div>{DEFAULT_MESSAGE}</div>}
                 {(matchedAddress !== null && !error) && (
-                    <div>
-                        <div>Districts for <strong>{matchedAddress}</strong>:</div>
-                        <div>{mappedDistricts.usHouse}</div>
-                        <div>{mappedDistricts.psc}</div>
-                        <div>{mappedDistricts.mtHouse}</div>
-                        <div>{mappedDistricts.mtSenate}</div>
-                        <a onClick={reset}>Reset</a>
+                    <div css={resultsContainerStyle}>
+                        <div css={headerStyle}>
+                            <div>Districts for <strong>{matchedAddress}</strong>:</div>
+                            <a onClick={reset} css={resetStyle}>Reset</a>
+                        </div>
+                        <div css={resultsContainerInnerStyle}>
+                            <div css={distResStyle}>{mappedDistricts.usHouse}</div>
+                            <div css={distResStyle}>{mappedDistricts.mtHouse}</div>
+                            <div css={distResStyle}>{mappedDistricts.mtSenate}</div>
+                            <div css={distResStyle}>{mappedDistricts.psc}</div>
+                        </div>
                     </div>
                 )}
             </div>
@@ -84,6 +95,52 @@ export default function AddressLookup({
     );
 }
 
+
+const resultsContainerStyle = css`
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+`;
+
+const headerStyle = css`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 10px;
+`;
+
+const resetStyle = css`
+    cursor: pointer;
+    background: var(--link);
+    color: white;
+    padding: 3px 10px 3px 10px;
+    transition: box-shadow 0.3s ease;
+    &:hover {
+        box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1);
+    }
+`;
+
+const resultsContainerInnerStyle = css`
+    display: flex;
+    gap: 10px;
+    flex-wrap: wrap;
+    justify-content: space-between;
+`;
+
+const distResStyle = css`
+    flex: 1 1 23%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    min-width: 150px;
+    max-width: 23%;
+    padding: 10px;
+    border: 1px solid var(--gray4);
+    background-color: var(--gray0);
+    border-radius: 5px;
+    text-align: center;
+    box-sizing: border-box;
+`;
 const errorStyle = css`
     background-color: #ffdddd;
     color: #d8000c;
@@ -139,16 +196,18 @@ const lookupStyle = css`
         background-color: var(--link);
     }
 `
-const districtResultsStyle = css`
-    padding: 10px;
-    border-radius: 5px;
-    margin-bottom: 10px;
-    font-size: 16px;
-    font-weight: bold;
-    display: flex;
+
+const buttonStyle = css`
+    flex: 1 1 auto;
+    background-color: var(--tan5);
+    color: white;
+    font-weight: normal;
+    transition: background-color 0.3s ease;
+    text-align: center;
+    padding: 0.5em 1em;
+    width: 120px;
+    display: inline-flex;
     align-items: center;
-    white-space: pre-wrap;
-    svg {
-        margin-right: 10px;
-    }
+    justify-content: center;
+    white-space: nowrap;
 `;
