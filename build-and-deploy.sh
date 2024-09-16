b1081e726a58aefe83de6987523e87a73330a1f4
Jacob Olness <jolness1@gmail.com>
2024-09-16 10:55:25 -0600

    update build-and-deploy.sh to use new cloudfront distribution

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -16,5 +16,9 @@ node process/make-candidate-list.js
 npm run build
 
 # Deploy
-aws s3 sync build s3://apps.montanafreepress.org/election-guide-2024 --delete
-aws cloudfront create-invalidation --distribution-id E3LVPS3XLJHLL5 --paths "/election-guide-2024/*"
+aws s3 sync build s3://projects.montanafreepress.org/election-guide-2024 --delete
+aws cloudfront create-invalidation --distribution-id E1G7ISX2SZFY34 --paths "/election-guide-2024/*"
+
+# Old Deploy for Eric's AWS/apps.montanafreepress.org
+# aws s3 sync build s3://apps.montanafreepress.org/election-guide-2024 --delete
+# aws cloudfront create-invalidation --distribution-id E3LVPS3XLJHLL5 --paths "/election-guide-2024/*"
