56b9b5506514fb55138c249b3bdd39999604b57c
Jacob Olness <jolness1@gmail.com>
2024-09-19 12:19:18 -0600

    testing

--- next.config.mjs
+++ next.config.mjs
@@ -4,8 +4,10 @@ const isProd = process.env.NODE_ENV === 'production'
 const nextConfig = {
     output: 'export',
     distDir: 'build',
+    // TODO - Change this before it goes live
+    assetPrefix: isProd ? ' http://projects.montanafreepress.org.s3-website-us-west-2.amazonaws.com/' : undefined,
     // assetPrefix: isProd ? 'https://apps.montanafreepress.org/election-guide-2024' : undefined,
-    assetPrefix: isProd ? 'https://projects.montanafreepress.org/election-guide-2024' : undefined,
+    // assetPrefix: isProd ? 'https://projects.montanafreepress.org/election-guide-2024' : undefined,
     basePath: '/election-guide-2024',
     trailingSlash: true,
     compiler: {
@@ -16,8 +18,10 @@ const nextConfig = {
         remotePatterns: [
             {
                 protocol: 'https',
+                // TODO - Change this before it goes live
+                hostname: 'http://projects.montanafreepress.org.s3-website-us-west-2.amazonaws.com/',
                 // hostname: 'apps.montanafreepress.org',
-                hostname: 'projects.montanafreepress.org',
+                // hostname: 'projects.montanafreepress.org',
                 port: '',
                 pathname: '/maps/legislative-districts/**',
             }
