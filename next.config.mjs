3ec913d9722cb0815028a29560d384440c9c9d4b
Jacob Olness <jolness1@gmail.com>
2024-10-09 15:55:30 -0600

    saving changes

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,7 @@ const nextConfig = {
     output: 'export',
     distDir: 'build',
     // TODO - Change this before it goes live
-    assetPrefix: isProd ? ' http://projects.montanafreepress.org.s3-website-us-west-2.amazonaws.com/' : undefined,
+    assetPrefix: isProd ? ' http://projects.montanafreepress.org.s3-website-us-west-2.amazonaws.com/election-guide-2024' : undefined,
     // assetPrefix: isProd ? 'https://apps.montanafreepress.org/election-guide-2024' : undefined,
     // assetPrefix: isProd ? 'https://projects.montanafreepress.org/election-guide-2024' : undefined,
     basePath: '/election-guide-2024',
